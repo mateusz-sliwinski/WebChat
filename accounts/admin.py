f70e15044c9839380f37ae1c88cb6f5ab359c848
Mati1432 <mateuszsliwinski9@gmail.com>
2023-01-31 16:14:07 +0100

    create model for user and create superuser while migrations and add migrations

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,25 @@
+"""Admin.py files."""
+# Django
 from django.contrib import admin
 
-# Register your models here.
+# Project
+from accounts.models import Users
+
+
+@admin.register(Users)
+class UsersAdmin(admin.ModelAdmin):  # noqa D101
+    list_display = [
+        'first_name',
+        'last_name',
+        'email',
+    ]
+    list_filter = [
+        'last_name',
+    ]
+    list_display_links = (
+        'email',
+    )
+    list_editable = [
+        'first_name',
+        'last_name',
+    ]
