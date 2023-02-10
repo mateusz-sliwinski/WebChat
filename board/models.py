c069fdefa450056d6147d81d573299007c5ad4af
Mati1432 <mateuszsliwinski9@gmail.com>
2023-02-10 11:14:50 +0100

    create new model

--- board/models.py
+++ board/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from accounts.models import Users
+
 
 # Create your models here.
 class Post(models.Model):
@@ -7,7 +9,17 @@ class Post(models.Model):
     title = models.CharField(max_length=100, blank=True, default='')
     image = models.ImageField(blank=True, null=True)
     body = models.TextField(blank=True, default='')
-    owner = models.ForeignKey('Users', related_name='posts', on_delete=models.CASCADE)
+    owner = models.ForeignKey(Users, related_name='posts', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['created']
+
+
+class Comment(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    body = models.TextField(blank=False)
+    owner = models.ForeignKey(Users, related_name='comments', on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
 
     class Meta:
         ordering = ['created']
