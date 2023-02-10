c8585100366b8af60afd0042b2a2674c3f087be8
Mati1432 <mateuszsliwinski9@gmail.com>
2023-02-10 11:15:45 +0100

    add serializer for post and comment

---
+++ board/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+
+from accounts.models import Users
+from board.models import Post, Comment
+
+
+class PostSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner')
+    comments = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'body', 'owner']
+
+
+class UsersPostSerializer(serializers.ModelSerializer):
+    posts = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    comments = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+
+    class Meta:
+        model = Users
+        fields = ['id', 'username', 'posts']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'body', 'owner', 'post']
