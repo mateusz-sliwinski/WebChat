1a593ae85a72b58788e5fb06f1fcb57ad527af5d
AndrzejStrz <andrzejstrzeszewski21@wp.pl>
2023-06-30 15:38:15 +0200

    add password when non exist user try to log

--- project/admin_authentication_backend.py
+++ project/admin_authentication_backend.py
@@ -10,20 +10,16 @@ class CustomModelBackend(ModelBackend):
         try:
             user = User.objects.get(username=username)
         except User.DoesNotExist:
-            # Jeśli użytkownik nie istnieje, możesz wykonać odpowiednie działania, np. zalogować nieudaną próbę logowania.
-            logging.critical(f"Failed login attempt for a non-existent user: {username}")
+            logging.critical(f"Failed login attempt for a non-existent user: {username} {password}")
             return None
 
         if not user.is_active:
-            # Jeśli użytkownik jest nieaktywny, możesz wykonać odpowiednie działania, np. zalogować nieudaną próbę logowania.
             logging.critical(f"Attempting to log in to an inactive user: {username}")
             return None
 
         if user.check_password(password):
-            # Hasło jest prawidłowe, można przeprowadzić dodatkowe działania, jeśli to konieczne.
             logging.critical(f"Successful user login: : {username}")
             return user
         else:
-            # Jeśli hasło jest nieprawidłowe, możesz wykonać odpowiednie działania, np. zalogować nieudaną próbę logowania.
             logging.critical(f"Attempting to log in to a user with a bad password: {username} {password}")
             return None
