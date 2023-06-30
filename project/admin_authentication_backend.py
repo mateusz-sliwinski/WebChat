716302967f4f832c3de0d7ceff84d289cc3a9ed4
AndrzejStrz <andrzejstrzeszewski21@wp.pl>
2023-06-30 15:00:58 +0200

    log for admin's login

--- project/admin_authentication_backend.py
+++ project/admin_authentication_backend.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.contrib.auth.backends import ModelBackend
 from django.contrib.auth import get_user_model
 
@@ -9,16 +11,19 @@ class CustomModelBackend(ModelBackend):
             user = User.objects.get(username=username)
         except User.DoesNotExist:
             # Jeśli użytkownik nie istnieje, możesz wykonać odpowiednie działania, np. zalogować nieudaną próbę logowania.
+            logging.info(f"Failed login attempt for a non-existent user: {username}")
             return None
 
         if not user.is_active:
             # Jeśli użytkownik jest nieaktywny, możesz wykonać odpowiednie działania, np. zalogować nieudaną próbę logowania.
-
+            logging.info(f"Attempting to log in to an inactive user: {username}")
             return None
 
         if user.check_password(password):
             # Hasło jest prawidłowe, można przeprowadzić dodatkowe działania, jeśli to konieczne.
+            logging.info(f"Successful user login: : {username}")
             return user
         else:
             # Jeśli hasło jest nieprawidłowe, możesz wykonać odpowiednie działania, np. zalogować nieudaną próbę logowania.
+            logging.info(f"Attempting to log in to a user with a bad password: {username}")
             return None
