870a676fa26800308000fa2dfe87cc966e93bc2a
pawel9009 <pawel.b512222@gmail.com>
2023-07-03 17:18:55 +0200

    update consumers and add reconecting websocket

--- chat/models.py
+++ chat/models.py
@@ -1,4 +1,17 @@
 # Django
 from django.db import models
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 # Create your models here.
+
+class Message(models.Model):
+    author = models.ForeignKey(User, related_name='author_message', on_delete=models.CASCADE)
+    content = models.TextField(max_length=1000)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.author.name}'
+
+    def last_10_messages(self):
+        return Message.objects.order_by('-timestamp').all()[:10]
