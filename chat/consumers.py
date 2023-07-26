1693bb971fcd27271e09037c8674947eef46ee21
pawel9009 <pawel.b512222@gmail.com>
2023-07-26 17:48:34 +0200

    change models and update consumers

--- chat/consumers.py
+++ chat/consumers.py
@@ -3,17 +3,26 @@ import json
 from django.contrib.auth import get_user_model
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
-from .models import Message, Chat
-
+from .models import ChatMessage, Chat, Participant
+from django.shortcuts import get_object_or_404
 from accounts.models import Users
+
+def get_user_contact(username):
+    user = get_object_or_404(Users, username=username)
+    return get_object_or_404(Participant, user=user)
+
+def get_current_chat(chatId):
+    return get_object_or_404(Chat, id=chatId)
 
 
 class ChatConsumer(WebsocketConsumer):
 
     def fetch_messages(self, data):
-        obj = Chat()
-        messages = obj.last_10_messages()
-        print(messages)
+
+        messages = ChatMessage.objects.all().filter(participant__chat_id=1)
+        print(len(messages))
+        for x in messages:
+            print('mess', x.content)
         content = {
             'command': 'messages',
             'messages': self.messages_to_json(messages)
@@ -21,15 +30,18 @@ class ChatConsumer(WebsocketConsumer):
         self.send_message(content)
 
     def new_message(self, data):
-        author = data['from']
-        author_user = Users.objects.filter(username=author)[0]
-        message = Message.objects.create(
-            author=author_user,
+        print(data)
+
+        author_user = get_user_contact(data['from'])
+        print(author_user)
+        message = ChatMessage.objects.create(
+            participant=author_user,
             content=data['message'])
         content = {
             'command': 'new_message',
             'message': self.message_to_json(message)
         }
+
         return self.send_chat_message(content)
 
     def messages_to_json(self, messages):
@@ -40,7 +52,7 @@ class ChatConsumer(WebsocketConsumer):
 
     def message_to_json(self, message):
         return {
-            'author': message.author.username,
+            'participant': message.participant.user.username,
             'content': message.content,
             'timestamp': str(message.timestamp)
         }
