219b17b2edcaee65f0023b786c73bd85915ae8b6
AndrzejStrz <andrzejstrzeszewski21@wp.pl>
2023-08-16 11:26:00 +0200

    req changed packages, typing of functions

--- project/utils.py
+++ project/utils.py
@@ -5,18 +5,18 @@ from dotenv import load_dotenv
 import os
 
 
-def get_password_data():
+def get_password_data() -> [str, bytes]:
     load_dotenv()
-    PASSWORD = os.environ.get('PASSWORD')
-    PASSWORD_SALT = bytes(os.environ.get('PASSWORD_SALT'), encoding='utf-8')
+    password = os.environ.get('PASSWORD')
+    password_salt = bytes(os.environ.get('PASSWORD_SALT'), encoding='utf-8')
 
-    return PASSWORD, PASSWORD_SALT
+    return password, password_salt
 
 
-def string_to_code(message: str):
-    PASSWORD, PASSWORD_SALT = get_password_data()
+def string_to_code(message: str) -> [bytes, bytes]:
+    password, password_salt = get_password_data()
 
-    key = PBKDF2(PASSWORD, PASSWORD_SALT, dkLen=32)
+    key = PBKDF2(password, password_salt, dkLen=32)
     cipher = AES.new(key, AES.MODE_CBC)
     encrypted_message = cipher.encrypt(pad(bytes(message, encoding='utf-8'), AES.block_size))
     initialization_vector = cipher.iv
@@ -24,11 +24,11 @@ def string_to_code(message: str):
     return encrypted_message, initialization_vector
 
 
-def code_to_string(encrypted_message: bytes, initialization_vector: bytes):
-    PASSWORD, PASSWORD_SALT = get_password_data()
+def code_to_string(encrypted_message: bytes, initialization_vector: bytes) -> str:
+    password, password_salt = get_password_data()
 
-    key = PBKDF2(PASSWORD, PASSWORD_SALT, dkLen=32)
+    key = PBKDF2(password, password_salt, dkLen=32)
     cipher = AES.new(key, AES.MODE_CBC, iv=initialization_vector)
     decrypted_message = unpad(cipher.decrypt(encrypted_message), AES.block_size)
 
-    return decrypted_message
+    return str(decrypted_message)
